d4e86f28eb89e77c8b4e31e3a7ae19c328cdcc9e
sookee <oasookee@gmail.com>
2013-01-21 11:03:32 +0000

    release-candidate

--- configure-release.sh
+++ configure-release.sh
@@ -4,8 +4,10 @@ GCC_HOME=${HOME}/app/gcc-4.7.0
 
 top_dir=$(pwd)
 
+SKIVVY_INCLUDES="-I../../../skivvy/src/include/"
+
 export CXX="${GCC_HOME}/bin/g++"
-export CXXFLAGS="-g0 -O3 -std=gnu++11 -Wno-unused-parameter -Wno-unused-variable"
+export CXXFLAGS="$SKIVVY_INCLUDES -g0 -O3 -std=gnu++11 -Wno-unused-parameter -Wno-unused-variable"
 
 mkdir -p $top_dir/build-release
 cd $top_dir/build-release
