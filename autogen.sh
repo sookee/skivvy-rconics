a90946b0228f329627e88bc2fe09f1382fad9f34
sookee <oasookee@gmail.com>
2013-01-26 13:30:42 +0000

    Fixed missing folder

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,4 @@
 #! /bin/sh
 mkdir -p m4
+mkdir -p config
 autoreconf --force --install -I config -I m4
-
-#libtoolize --force --copy \
-#&& aclocal \
-#&& autoheader \
-#&& automake -c --gnu --add-missing --force-missing \
-#&& autoconf
