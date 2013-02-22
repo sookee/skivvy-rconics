8c1020817643906963ecfcfe6801e3f1308f12f6
sookee <oasookee@gmail.com>
2013-02-22 16:22:00 +0000

    Fix: crasher #40 - replaced rcon connect code.

--- src/plugin-rconics-rcon.cpp
+++ src/plugin-rconics-rcon.cpp
@@ -122,50 +122,125 @@ using namespace skivvy::types;
 
 #define TIMEOUT 1000
 
+//bool aocom(const str& cmd, str_vec& packets, const str& host, int port
+//	, siz wait = TIMEOUT)
+//{
+//	int cs = socket(PF_INET, SOCK_DGRAM, 0);
+//	int cs_flags = fcntl(cs, F_GETFL, 0);
+//
+//	if(cs <= 0)
+//	{
+//		log(strerror(errno));
+//		return false;
+//	}
+//
+//	sockaddr_in sin;
+//	hostent* he = gethostbyname(host.c_str());
+//	std::copy(he->h_addr, he->h_addr + he->h_length
+//		, reinterpret_cast<char*>(&sin.sin_addr.s_addr));
+//	sin.sin_family = AF_INET;
+//	sin.sin_port = htons(port);
+//
+//	fcntl(cs, F_SETFL, cs_flags | O_NONBLOCK);
+//
+//	st_time_point timeout = st_clk::now() + std::chrono::milliseconds(wait);
+//
+//	int n = 0;
+//	while((n = connect(cs, (struct sockaddr *) &sin, sizeof(sin))) < 0 && errno == EINPROGRESS)
+//	{
+//		if(st_clk::now() > timeout)
+//		{
+//			log("socket timed out connecting to: " << host << ":" << port);
+//			return false;
+//		}
+//		std::this_thread::yield();
+//	}
+//
+//	if(n < 0)
+//	{
+//		log(strerror(errno) << ": " << host << ":" << port);
+//		return false;
+//	}
+//
+//	fcntl(cs, F_SETFL, cs_flags);
+//
+//	const str msg = "\xFF\xFF\xFF\xFF" + cmd;
+//
+//	if((n = send(cs, msg.c_str(), msg.size(), 0)) < 0 || n < (int)msg.size())
+//	{
+//		log("cs send: " << strerror(errno));
+//		return false;
+//	}
+//
+//	packets.clear();
+//
+//	char buf[2048];
+//
+//	n = sizeof(buf);
+//	while(n == sizeof(buf))
+//	{
+//		while((n = recv(cs, buf, sizeof(buf), MSG_DONTWAIT)) ==  -1 && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
+//		{
+//			if(st_clk::now() > timeout)
+//			{
+//				log("socket timed out connecting to: " << host << ":" << port);
+//				return false;
+//			}
+//			std::this_thread::yield();
+//		}
+//		if(n < 0)
+//			log("cs recv: " << strerror(errno));
+//		if(n > 0)
+//			packets.push_back(str(buf, n));
+//	}
+//
+//	close(cs);
+//
+//	return true;
+//}
+
 bool aocom(const str& cmd, str_vec& packets, const str& host, int port
 	, siz wait = TIMEOUT)
 {
-	int cs = socket(PF_INET, SOCK_DGRAM, 0);
-	int cs_flags = fcntl(cs, F_GETFL, 0);
+	addrinfo hints;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6
+	hints.ai_socktype = SOCK_DGRAM;
 
-	if(cs <= 0)
+	addrinfo* res;
+	if(int status = getaddrinfo(host.c_str(), std::to_string(port).c_str(), &hints, &res) != 0)
 	{
-		log(strerror(errno));
+		log(gai_strerror(status));
 		return false;
 	}
-
-	sockaddr_in sin;
-	hostent* he = gethostbyname(host.c_str());
-	std::copy(he->h_addr, he->h_addr + he->h_length
-		, reinterpret_cast<char*>(&sin.sin_addr.s_addr));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(port);
-
-	fcntl(cs, F_SETFL, cs_flags | O_NONBLOCK);
 
 	st_time_point timeout = st_clk::now() + std::chrono::milliseconds(wait);
 
-	int n = 0;
-	while((n = connect(cs, (struct sockaddr *) &sin, sizeof(sin))) < 0 && errno == EINPROGRESS)
+	// try to connect to each
+	int cs;
+	addrinfo* p;
+	for(p = res; p; p = p->ai_next)
 	{
-		if(st_clk::now() > timeout)
-		{
-			log("socket timed out connecting to: " << host << ":" << port);
-			return false;
-		}
-		std::this_thread::yield();
+		if((cs = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+			continue;
+		if(!connect(cs, p->ai_addr, p->ai_addrlen))
+			break;
+		::close(cs);
 	}
 
-	if(n < 0)
+	freeaddrinfo(res);
+
+	if(!p)
 	{
-		log(strerror(errno) << ": " << host << ":" << port);
+		log("aocom: failed to connect: " << host << ":" << port);
 		return false;
 	}
 
-	fcntl(cs, F_SETFL, cs_flags);
+	// cs good
 
 	const str msg = "\xFF\xFF\xFF\xFF" + cmd;
 
+	int n = 0;
 	if((n = send(cs, msg.c_str(), msg.size(), 0)) < 0 || n < (int)msg.size())
 	{
 		log("cs send: " << strerror(errno));
